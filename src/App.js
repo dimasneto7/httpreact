97401f685d449e7c7a4d259cbb1ebc19d7b5cb79
Dimas Neto <dimas_fcn@outlook.com>
2023-01-11 15:54:48 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig, loading } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -34,17 +34,19 @@ function App() {
       price,
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    const addedProduct = await res.json();
+    // const addedProduct = await res.json();
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
@@ -53,14 +55,17 @@ function App() {
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
-        {items &&
-          items.map((product) => (
-            <li key={product.id}>
-              {product.name} - {product.price}
-            </li>
-          ))}
-      </ul>
+      {loading && <p>Carregando dados...</p>}
+      {!loading && (
+        <ul>
+          {items &&
+            items.map((product) => (
+              <li key={product.id}>
+                {product.name} - R$ {product.price}
+              </li>
+            ))}
+        </ul>
+      )}
       <div className="add-product">
         <form onSubmit={handleSubimit}>
           <label>
@@ -81,7 +86,8 @@ function App() {
               onChange={(e) => setPrice(e.target.value)}
             />
           </label>
-          <input type="submit" value="Criar" />
+          {loading && <input type="submit" value="Aguarde" disabled />}
+          {!loading && <input type="submit" value="Criar" />}
         </form>
       </div>
     </div>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,17 +3,57 @@ import { useState, useEffect } from "react";
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod("POST");
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
+
       const res = await fetch(url);
 
       const json = await res.json();
 
       setData(json);
+
+      setLoading(false);
     };
 
     fetchData();
-  }, [url]);
+  }, [url, callFetch]);
 
-  return { data };
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+
+    httpRequest();
+  }, [config, method, url]);
+
+  return { data, httpConfig, loading };
 };
